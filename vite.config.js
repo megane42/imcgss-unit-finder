17483ed3088b1458d322517a74589beff70aa6d6
megane42 <h.kzm.gm@gmail.com>
2025-02-10 01:10:38 +0900

    Fixed vite config for github pages
    https://vite.dev/guide/static-deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import elmPlugin        from 'vite-plugin-elm'
 
 export default defineConfig({
+  base: '/imcgss-unit-finder/',
   plugins: [
     elmPlugin()
   ]
