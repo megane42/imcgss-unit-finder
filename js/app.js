9fcc3100d385c95f5da8c815b53c6b7013b88841
megane42 <h.kzm.gm@gmail.com>
2025-02-09 19:11:34 +0900

    Fixed folder structures

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-import './app.css'
-import { Elm } from './src/Imcgss/UnitFinder/Main.elm'
+import '../css/app.css'
+import { Elm } from '../src/Imcgss/UnitFinder/Main.elm'
 
 const root = document.querySelector("#app");
 const elmApp = Elm.Imcgss.UnitFinder.Main.init({ node: root })
